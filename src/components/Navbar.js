958252254183cc1b187c16ef8397190a23c27bfe
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-03-01 14:57:18 -0300

    Navlinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
 import './Navbar.css';
 
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">Sobre</Link>
+      <NavLink to="/" 
+        // className={({isActive}) => (isActive ? 'ativo' : 'nao-ativo')}
+        >Home
+      </NavLink>
+      <NavLink to="/about">Sobre</NavLink>
     </nav>
   )
 }

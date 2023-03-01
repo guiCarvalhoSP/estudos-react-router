05095358eadfd05611c0ee04c2693a6c50fa84f7
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-03-01 11:06:07 -0300

    Implementando react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import About from './pages/About';
+import Home from './pages/Home';
+
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

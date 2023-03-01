0bdd5a291d63c931d3026ed15b7d04973bca3cba
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-03-01 14:00:57 -0300

    Criação da tela de home e navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
 import About from './pages/About';
 import Home from './pages/Home';
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <h1>React Router</h1>
       <BrowserRouter>
+        <Navbar></Navbar>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
